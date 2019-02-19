2b0c50c058b279d7edd5eb55a49693526c5ea7a7
danglotb <benjamin.danglot@inria.fr>
2019-02-19 17:28:39 +0100

    update script to run

--- src/main/bash/march-2019/run.sh
+++ src/main/bash/march-2019/run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
+# Copy our configured script files
+cp src/main/bash/march-2019/preprocess.sh code2vec/preprocess.sh
+cp src/main/bash/march-2019/train.sh code2vec/train.sh
+
 # Clone code2vec
 git clone https://github.com/tech-srl/code2vec
 cd code2vec
 git reset --hard a7873314a1066c945048ef25f8f843e4c932c207
-
-# Copy our configured script files
-cp src/main/bash/march-2019/preprocess.sh code2vec/preprocess.sh
-cp src/main/bash/march-2019/train.sh code2vec/train.sh
 
 # retrieve the raw-benchmark from Zenodo
 cd code2vec/
@@ -15,7 +15,7 @@ wget https://zenodo.org/record/2567792/files/benchmark.zip
 unzip benchmark.zip
 
 # preprocess
-source preprocess.sh
+sh preprocess.sh
 
 # train
-source train.sh
+sh train.sh
